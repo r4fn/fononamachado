3a754c9574b53bfe339c32cd0b3081a838c38b12
eduard0vieira <eduardohvieira05@gmail.com>
2026-03-25 08:54:50 -0300

    chore: simplify next.config.ts by removing unnecessary configurations

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
