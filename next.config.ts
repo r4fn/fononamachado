522af81845d79bf98a2490cb9d62936efb1576fe
eduard0vieira <eduardohvieira05@gmail.com>
2026-03-24 23:46:46 -0300

    chore: update next.config.ts formatting and remove deploy.yml workflow

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
